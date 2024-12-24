ce8481bab10281d1472805541b8baff62fbd6142
ackPeng <1143590135@qq.com>
2024-12-24 13:40:58 +0800

    80 test 5

--- RHUartDriver.cpp
+++ RHUartDriver.cpp
@@ -122,7 +122,8 @@ template class RHUartDriver<HardwareSerial>;
     template class RHUartDriver<SoftwareSerial>;
 #endif
 
+
 #if defined(ARDUINO_ARCH_RP2040) || defined(ARDUINO_ARCH_RP2350) || defined(ARDUINO_XIAO_RA4M1)
     #include <SoftwareSerial.h>
-    template class RH_RF95<SoftwareSerial>;
+    template class RHUartDriver<SoftwareSerial>;
 #endif
